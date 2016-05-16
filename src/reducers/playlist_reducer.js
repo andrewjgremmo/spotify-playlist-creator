a0c914f20a99173351e0e19cda9f5f548f490594
Andrew Gremmo <andrewjgremmo@gmail.com>
2016-05-15 22:32:41 -0400

    Fix dynamic playlist title generation to account for deletion resulting less than 3 valid top artists

--- src/reducers/playlist_reducer.js
+++ src/reducers/playlist_reducer.js
@@ -16,7 +16,8 @@ const INITIAL_STATE = {
 }
 
 export default function(state = INITIAL_STATE, action) {
-  let artist;
+  let artist, artists;
+
   switch(action.type) {
     case FETCH_SONG:
       artist = action.payload.artist.name;
@@ -31,15 +32,17 @@ export default function(state = INITIAL_STATE, action) {
       };
     case REMOVE_SONG:
       artist = action.payload.artist.name;
+      artists = {
+        ...state.artists,
+        [artist]: state.artists[artist] ? state.artists[artist] - 1 : 0
+      };
+
       return { ...state,
         songs: state.songs.filter(
           (song) => song.song.id != action.payload.song.id
         ),
-        artists: {
-          ...state.artists,
-          [artist]: state.artists[artist] ? state.artists[artist] - 1 : 0
-        },
-        topArtists: getTopArtists(state.artists),
+        artists: artists,
+        topArtists: getTopArtists(artists),
         saved: false
       }
     case REMOVE_ALL_SONGS:
@@ -52,7 +55,10 @@ export default function(state = INITIAL_STATE, action) {
 }
 
 function getTopArtists(artists) {
-  return take(Object.keys(artists).sort(function(a,b) {
-    return artists[b] - artists[a];
-  }), 3);
+  //Return the artists with the 3 highest number of occurrence, reject any not > 0
+  return take(Object.keys(artists).filter(
+      (artist) => artists[artist] > 0
+    ).sort(function(a,b) {
+      return artists[b] - artists[a];
+    }), 3);
 }
